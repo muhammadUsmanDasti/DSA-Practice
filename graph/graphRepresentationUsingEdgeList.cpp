33c35aa604adfa718899115629774e39126055d2
Muhammad Usman Dasti <muhammadusmandasti07@gmail.com>
2026-05-24 16:25:28 +0500

    some modification in edge list

--- graph/graphRepresentationUsingEdgeList.cpp
+++ graph/graphRepresentationUsingEdgeList.cpp
@@ -1,2 +1,27 @@
 #include<iostream>
+#include<vector>
 using namespace std;
+
+
+class edge{
+    int startVertex;
+    int endVertex;
+    int weight;
+};
+int main(){
+    vector<string> vertexList;
+    vector<edge> edgeList;
+    int v,e;
+    cout<<"Enter the number of vertices: ";
+    cin>>v;
+    cout<<"Enter the number of edges: ";
+    cin>>e;
+    int start,end;
+    for(int i=0;i<e;i++){
+        
+        cout<<"Enter the edge start node: ";
+        cin>>start;
+        edgeList[i].startVertex =
+    }
+    return 0;
+}
